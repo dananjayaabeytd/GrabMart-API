4f248bc9a68c848ee1ef5f7adeefa0c396dac6eb
Tharindu Dananjaya Abenayaka <155418943+dananjayaabeytd@users.noreply.github.com>
2024-12-29 15:12:19 +0530

    connect db

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import dbConnect from '../config/dbConnect.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
+// Connect to database
+dbConnect();
 const app = express();
 
 export default app;
